25f3a6e79bac4ec6b803126b3b9e0de9a3ab0b7d
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2021-11-28 22:40:40 -0500

    Added the function for the crust price and gluten option

--- script.js
+++ script.js
@@ -7,3 +7,46 @@ function Pizza(type, size, crust, topping, total){
   this.topping = topping;
   this.total = total;
 }
+$(document).ready(function(){
+  //proceed button
+  $("button#proceed-btn").click(function(event){
+      
+
+      var pizzaName = $("#type option:selected").val();
+      var pizzaSize = $("#size option:selected").val();
+      var crustType = $("#crust option:selected").val();
+      var selectedTopping = [];
+      $.each($("input[name='toppings']:checked"), function(){
+        selectedTopping.push($(this).val());
+        
+      });
+      console.log(selectedTopping.join(" , "));
+
+
+      switch(pizzaSize){
+        case "0": price = 0;
+        break;
+        case "large": price = 1200;
+        console.log(price);
+        break;
+        case "medium": price = 900;
+        console.log(price);
+        break;
+        case "small": price = 600;
+        console.log(price);
+        default: console.log("error")
+      }
+      switch(crustType){
+        case "0": crustPrice = 0;
+        break;
+        case "crispy": crustPrice = 100;
+        console.log(crustPrice);
+        break;
+        case "stuffed": crustPrice = 200;
+        console.log(crustPrice);
+        break;
+        case "gluten-free": crustPrice = 300;
+        console.log(crustPrice);
+        default: console.log("No Price")
+      }
+      
